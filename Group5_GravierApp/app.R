5780eea38808d943e8f1947d9de46b47588b5df8
LucilleAugusta <s184250@student.dtu.dk>
2021-04-12 08:50:16 +0000

    code

---
+++ Group5_GravierApp/app.R
@@ -0,0 +1,58 @@
+#
+    
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Manhattan-plot, Gravier Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("Value",
+                        "log10(p-value)",
+                        min = -0.5,
+                        max = 3.5,
+                        value = 0.05)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        library("tidyverse")
+        data<- readRDS("gravier_data_long_nested.rds")
+        
+        data %>% 
+            ggplot(mapping = aes(x = fct_reorder(gene, neg_log10_p, .desc = TRUE), 
+                                 y = neg_log10_p,
+                                 color = identified_as)) +
+            geom_point() + 
+            geom_hline(yintercept = - log10(input$Value), linetype = "dashed") +
+            theme_classic(base_size = 8, base_family = "Avenir") +
+            theme(legend.position = "bottom", 
+                  axis.text.x = element_text(angle = 45 , vjust = 1, hjust = 1)) +
+            labs( x = "Gene", y = "Minus log10(p)")
+        
+        
+        # generate bins based on input$bins from ui.R
+        #x    <- faithful[, 2]
+        #bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        #hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- Intro.R
+++ Intro.R
@@ -1,2 +1,36 @@
-# Intro 
+# Loading and formating the data
+library("tidyverse")
+library("fs") 
+library("broom")
+load(file = "gravier.RData")
 
+gravier_data <- gravier$x %>% as.tibble() %>% 
+  mutate(outcome = as.integer(gravier$y)-1)
+
+gravier_data_long <- gravier_data %>% 
+  pivot_longer(cols = -outcome, names_to = "gene", values_to = "log2_expr_level")
+
+gravier_data_long_nested <- gravier_data_long %>%  
+  group_by(gene) %>% 
+  nest() %>%  ungroup()
+
+# takes 100 random genes in the data set
+set.seed(1234)
+gravier_data_long_nested <- gravier_data_long_nested %>%  sample_n(100) 
+
+gravier_data_long_nested <- gravier_data_long_nested %>%  mutate(mdl = map(data, ~ glm(outcome ~ log2_expr_level, 
+                                                                                       data = .x, 
+                                                                                       family = binomial(link = "logit"))))
+gravier_data_long_nested <- gravier_data_long_nested %>%  
+  mutate(mdl_tidy = map(mdl, tidy, conf.int = TRUE)) %>% 
+  unnest(mdl_tidy)
+
+gravier_data_long_nested <- gravier_data_long_nested %>% filter(term == "log2_expr_level")
+
+gravier_data_long_nested <- gravier_data_long_nested %>%
+  mutate(identified_as = case_when( p.value < 0.05 ~ "Significant",
+                                    TRUE ~ "Non-significant"))
+gravier_data_long_nested <- gravier_data_long_nested %>%
+  mutate(neg_log10_p = -log10(p.value))
+
+saveRDS(gravier_data_long_nested, "gravier_data_long_nested.rds")
